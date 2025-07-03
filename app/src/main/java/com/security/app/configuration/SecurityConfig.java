04f4e4b992e7fb587eb21c6a596835c6af9ff423
Chilaqui <comhector_@hotmail.com>
2025-07-02 23:38:54 -0600

    configuracion de el password encoder

--- app/src/main/java/com/security/app/configuration/SecurityConfig.java
+++ app/src/main/java/com/security/app/configuration/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -27,9 +29,15 @@ public class SecurityConfig {
             .formLogin(form -> form.disable());
     }
 
+    @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
 
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
