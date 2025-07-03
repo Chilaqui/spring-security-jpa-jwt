a6f6ee42a12cb7f12e4338a54765c2b26986869f
Chilaqui <comhector_@hotmail.com>
2025-07-02 23:37:15 -0600

    configuracion de el autentication Manager

--- app/src/main/java/com/security/app/configuration/SecurityConfig.java
+++ app/src/main/java/com/security/app/configuration/SecurityConfig.java
@@ -2,14 +2,18 @@ package com.security.app.configuration;
 
 import java.net.http.HttpClient;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
         http
             .csrf(csrf -> csrf.disable())
@@ -22,4 +26,10 @@ public class SecurityConfig {
             .httpBasic(Customizer.withDefaults())
             .formLogin(form -> form.disable());
     }
+
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+
+        return config.getAuthenticationManager();
+    }
+
 }
