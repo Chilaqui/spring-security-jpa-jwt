9441b7166e276918dab29a8c6af66e5b5a225c56
Chilaqui <comhector_@hotmail.com>
2025-07-04 11:17:14 -0600

    Creacion de la Key con la variable SECRET_KEY que contiene  el texto a convertir

--- app/src/main/java/com/security/app/jwt/JwtUtils.java
+++ app/src/main/java/com/security/app/jwt/JwtUtils.java
@@ -1,11 +1,20 @@
 package com.security.app.jwt;
 
+import java.security.Key;
+import java.security.Signature;
+import java.util.Date;
+
 import org.springframework.security.core.userdetails.UserDetails;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 public class JwtUtils {
 
-    public String generateToken(UserDetails userDetails){
+    private static final Key SECRET_KEY = Keys.hmacShaKeyFor("mi_clave_secreta_larga_y_constante".getBytes());
 
+    public String generateToken(UserDetails userDetails){
         return null;
     }
 
