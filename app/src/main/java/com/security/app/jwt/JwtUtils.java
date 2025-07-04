ce26f54609a3c3fe4c85f8a84bb5f3133eec31fc
Chilaqui <comhector_@hotmail.com>
2025-07-04 11:18:20 -0600

    Creacion de las variables de fecha para que sean creadas en el JWTs en el constructor

--- app/src/main/java/com/security/app/jwt/JwtUtils.java
+++ app/src/main/java/com/security/app/jwt/JwtUtils.java
@@ -15,7 +15,16 @@ public class JwtUtils {
     private static final Key SECRET_KEY = Keys.hmacShaKeyFor("mi_clave_secreta_larga_y_constante".getBytes());
 
     public String generateToken(UserDetails userDetails){
-        return null;
+        Date issuedAt = new Date();
+        Date expiration = new Date(issuedAt.getTime() + 1000 * 60 * 60); // 1 hora de expiracion 
+
+
+        return Jwts.builder()
+            .setSubject(userDetails.getUsername())//Nobre de el usuario
+            .setIssuedAt(issuedAt)
+            .setExpiration(expiration)
+            .signWith(SECRET_KEY, SignatureAlgorithm.HS256)
+            .compact();
     }
 
     public boolean validarToken(String token,UserDetails userDetails){
