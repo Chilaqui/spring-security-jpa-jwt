4711b00828db7bb067f2600b445188cbbc7e56eb
Hector <comhector_@hotmail.com>
2025-07-07 21:37:52 -0600

    Configuracion del el controller para integar jwt

--- app/src/main/java/com/security/app/controller/UserController.java
+++ app/src/main/java/com/security/app/controller/UserController.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.security.app.dto.AuthRequest;
 import com.security.app.model.User;
 import com.security.app.service.UserService;
+import com.security.app.service.auth.AuthService;
 
 @RestController
 @RequestMapping("/auth")
@@ -19,16 +21,26 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private AuthService authService;// Inyectamos el servicio de autenticación con JWT
+
     @PreAuthorize("hasRole('DEVELOPER')")
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody User user){
         return userService.registerUser(user);
     }
 
-    
-    @GetMapping("/login")
+    //Cambiando el login simple por la integracion de JWT
+    /* @GetMapping("/login")
     public ResponseEntity<String> login(@RequestBody User user){
         return ResponseEntity.ok(userService.login(user));
+    } */
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody AuthRequest authRequest){
+        //Se llama al servicio para autenticar y generar el JWT
+        String token = authService.login(authRequest.getUsername(), authRequest.getPassword());//Se llama al dto para que podamos recibir JSON
+        return ResponseEntity.ok(token);//Devuelve el token como respuesta
     }
 
     @PreAuthorize("hasRole('ADMIN')")
