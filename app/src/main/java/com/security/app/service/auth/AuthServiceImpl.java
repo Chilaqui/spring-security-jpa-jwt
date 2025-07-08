4a9678af7966523281193a8519f2b25edc98d75f
Hector <comhector_@hotmail.com>
2025-07-07 21:14:49 -0600

    comentando

--- app/src/main/java/com/security/app/service/auth/AuthServiceImpl.java
+++ app/src/main/java/com/security/app/service/auth/AuthServiceImpl.java
@@ -21,12 +21,16 @@ public class AuthServiceImpl implements AuthService {
     //Este es el método principal que implementa la lógica del login.Recibe el username y la password desde el cliente (Postman, frontend, etc.).
     @Override
     public String login(String username,String password){
+
+        // 1. Autenticar usuario usando Spring Security
         //Llama a authManager.authenticate(...), que revisa el usuario y la contraseña usando tu UserDetailsService.
         Authentication authentication = authManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));//Crea un objeto UsernamePasswordAuthenticationToken, que Spring usa para representar un intento de login.
 
+        // 2. Obtener los detalles del usuario autenticado
         //Si el usuario fue autenticado correctamente, Spring devuelve un objeto Authentication.
         UserDetails userDetails =(UserDetails) authentication.getPrincipal();//El getPrincipal() contiene los datos del usuario (que tú cargaste desde la base de datos).
 
+        // 3. Generar un token JWT y devolverlo
         //Usamos la clase JwtUtils para generar un token JWT con la información del usuario.
         return jwtUtils.generateToken(userDetails);
 
