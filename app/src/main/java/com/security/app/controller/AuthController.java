47aa05977409d3ffc7c5f44d011d1cf19f43510c
Hector <comhector_@hotmail.com>
2025-07-15 18:48:29 -0600

    Creamos las rutas correctamente, para poder usar el login y el register correctamente

--- app/src/main/java/com/security/app/controller/AuthController.java
+++ app/src/main/java/com/security/app/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.security.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,7 @@ public class AuthController {
     @Autowired
     private AuthService authService; // Inyectamos el servicio de autenticación con JWT
 
-    @PostMapping("/loginjwt")
+    @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
         // Se llama al servicio para autenticar y generar el JWT
         String token = authService.loginJwt(
@@ -27,7 +28,8 @@ public class AuthController {
         return ResponseEntity.ok(token);// Devuelve el token como respuesta
     }
 
-    @PostMapping("/registerjwt")
+    @PreAuthorize("hasRole('DEVELOPER')")
+    @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody RegisterRequest registerRequest) {
 
         String token = authService.registerJwt(
