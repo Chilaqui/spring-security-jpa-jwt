ecfdbe10b723ce78a44320964cec7d5051136c2a
Hector <comhector_@hotmail.com>
2025-07-15 18:36:03 -0600

    Quitamos las rutas que tienen login para evitar el manejo de login simple y con jwt

--- app/src/main/java/com/security/app/controller/UserController.java
+++ app/src/main/java/com/security/app/controller/UserController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.security.app.dto.AuthRequest;
 import com.security.app.model.User;
 import com.security.app.service.UserService;
 import com.security.app.service.auth.AuthService;
@@ -24,6 +22,8 @@ public class UserController {
     @Autowired
     private AuthService authService;// Inyectamos el servicio de autenticación con JWT
 
+    //Logins y Registrations simples, sin JWT
+
     @PreAuthorize("hasRole('DEVELOPER')")
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody User user){
@@ -31,16 +31,9 @@ public class UserController {
     }
 
     //Cambiando el login simple por la integracion de JWT
-    /* @GetMapping("/login")
+    @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody User user){
         return ResponseEntity.ok(userService.login(user));
-    } */
-
-    @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody AuthRequest authRequest){
-        //Se llama al servicio para autenticar y generar el JWT
-        String token = authService.login(authRequest.getUsername(), authRequest.getPassword());//Se llama al dto para que podamos recibir JSON
-        return ResponseEntity.ok(token);//Devuelve el token como respuesta
     }
 
     @PreAuthorize("hasRole('USER')")
