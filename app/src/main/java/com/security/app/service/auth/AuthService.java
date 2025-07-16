3a3f893e048798045f82b22860cb265e9b463eb6
Hector <comhector_@hotmail.com>
2025-07-15 18:13:01 -0600

    Registrando en AuthService para poder separar responsabilidades

--- app/src/main/java/com/security/app/service/auth/AuthService.java
+++ app/src/main/java/com/security/app/service/auth/AuthService.java
@@ -5,4 +5,9 @@ public interface AuthService {
 
     //El método tiene una única responsabilidad: autenticar al usuario y devolver un token JWT.
     String login(String username, String password);
+
+    // Método para registrar un nuevo usuario
+    // Este método recibe un nombre de usuario y una contraseña, y devuelve un token JWT si
+    String register(String username, String password); 
+    
 }
