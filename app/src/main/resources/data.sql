4351fa233633f7a832cd42b50ea58913ca906cf6
Hector <comhector_@hotmail.com>
2025-07-15 19:44:18 -0600

    Creamos data para que sea pueda inyectar directamente cuando la aplicacion levante

---
+++ app/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO user (user_name, password, role) VALUES
+('adminUser', '$2a$10$e0MYzXyjpJS7Pd0RVvHwHeFXc5j5VTgtrdSV6GB3oKXsd7t1YsaUa', 'ADMIN'),
+('developerUser', '$2a$10$4Rb3L5PqivEmE.7tTPJr3e1aQruPikZ8xW9RO1JYHd0Rk3/WV3zMu', 'DEVELOPER'),
+('managerUser', '$2a$10$eW5z0wuy6Uqa4zRIoPE5bOLxFowc5whFZJeQ9u0zP7HP3ZWy0Gj2G', 'MANAGER'),
+('normalUser', '$2a$10$eXcF9x0XUu0Bv4Yt9TR/2eQ6Krcz0qkKxYzFXK6rDgLwrx0vEpxme', 'USER');
